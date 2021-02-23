31949a97131c9aad4b188146a35ff10da296fce8
Vaibhav Mishra <mvaib2001@gmail.com>
2021-02-23 23:47:02 +0530

    Switch case using PHP

---
+++ switch.php
@@ -0,0 +1,109 @@
+<?php
+
+$sem = 3;
+switch ($sem) {
+    case 1: #FALLSEM2019-20
+        echo "LIST OF COURSES IN FIRST SEMESTER: <br><br>";
+        #A1 slot
+        echo "A1- CHY1701 ENGINEERING CHEMISTRY <br>";
+        #Chemistry LAB slot
+        echo "L53+L54- CHY1701 ENGINEERING CHEMISTRY <br>"; 
+        #B1 slot
+        echo "B1- EEE1001 BASIC ELECTRICAL AND ELECTRONICS ENGINEERING <br>";
+        #EEE LAB slot
+        echo "L39+L40- EEE1001 BASIC ELECTRICAL AND ELECTRONICS ENGINEERING <br>";
+        #C1 slot
+        echo "C1- CHY1002 ENVIRONMENTAL SCIENCES <br>";
+        #D1 slot
+        echo "D1- HUM1021 ETHICS AND VALUES <br>";
+        #F1 slot
+        echo "F1- STS1201 SOFT SKILLS <br>";
+        #G1 slot
+        echo "G1- MAT1011 CALCULUS FOR ENGINEERS <br>";  
+        #Calculus LAB slot
+        echo "L35+L36- MAT1011 CALCULUS FOR ENGINEERS <br>"; 
+        #Programming slot
+        echo "L31+L32+L41+L42+L55+L56- CSE1001 PROBLEM SOLVING AND PROGRAMMING <br>"; 
+        #English slot
+        echo "L43+L44+L57+L58- ENG2000 FOUNDATION ENGLISH 2 <br>";
+        break;
+
+    case 2: #WINSEM2019-20
+        echo "LIST OF COURSES IN SECOND SEMESTER: <br><br>";
+        #A2 slot
+        echo "A2- ITE1001 DIGITAL LOGIC AND MICROPROCESSOR <br>";
+        #DLM LAB slot
+        echo "L11+L12- ITE1001 DIGITAL LOGIC AND MICROPROCESSOR <br>"; 
+        #B2 slot
+        echo "B2- MAT2002 APPLICATION OF DIFFERENTIAL AND DIFFERENCE EQUATIONS <br>";
+        #AOD LAB slot
+        echo "L1+L2- MAT2002 APPLICATION OF DIFFERENTIAL AND DIFFERENCE EQUATIONS <br>";
+        #D2 slot
+        echo "D2- ITE1004 DATA STRUCTURES AND ALGORITHMS <br>";
+        #DSA LAB slot
+        echo "L27+L28- ITE1004 DATA STRUCTURES AND ALGORITHMS <br>";
+        #E2 slot
+        echo "E2- MAT2001 STATISTICS FOR ENGINEERS <br>";
+        #Statistics LAB slot
+        echo "L21+L22- MAT2001 STATISTICS FOR ENGINEERS <br>";
+        #F2 slot
+        echo "F2- STS1202 SOFT SKILLS <br>"; 
+        #Programming slot
+        echo "L15+L16+L19+L20+L29+L30- CSE1002 PROBLEM SOLVING AND OBJECT ORIENTED PROGRAMMING <br>"; 
+        #English slot
+        echo "L3+L4+L13+L14- ENG1901 TECHNICAL ENGLISH 1 <br>";
+        break;
+    case 3: #FALLSEM2020-21
+        echo "LIST OF COURSES IN THIRD SEMESTER: <br><br>";
+        #A1 slot
+        echo "A1- ITE1003 DATABASE MANAGEMENT SYSTEMS <br>";
+        #DBMS LAB slot
+        echo "L51+L52- ITE1003 DATABASE MANAGEMENT SYSTEMS <br>";
+        #DBMS J Component
+        echo "L51+L52- ITE1003 DATABASE MANAGEMENT SYSTEMS <br>"; 
+        #B2 slot
+        echo "B2- EEE1002 ELECTRIC CIRCUITS <br>";
+        #C1 slot
+        echo "C1- ITE1005 SOFTWARE ENGINEERING PRINCIPLES AND PRACTICES <br>";
+        #C2 slot
+        echo "C2- MAT1014 DISCRETE MATHEMATICS AND GRAPH THEORY <br>";
+        #D1 slot
+        echo "D1- ITE1014 HUMAN COMPUTER INTERACTION <br>";
+        #HCI J Component
+        echo "D1- ITE1014 HUMAN COMPUTER INTERACTION <br>";
+        #E1 slot
+        echo "E1- STS2201 SOFT SKILLS <br>";
+        #G1 slot
+        echo "G1- ITE1002- WEB TECHNOLOGIES <br>";
+        #Web Tech LAB slot
+        echo "L59+L60- ITE1002- WEB TECHNOLOGIES <br>";
+        #TG1 slot
+        echo "TG1- PHY1901 INTRODUCTION TO INNOVATIVE PROJECTS <br>";
+        break;
+    case 4: #WINSEM2020-21
+        echo "LIST OF COURSES IN FOURTH SEMESTER: <br><br>";
+        #A1 slot
+        echo "A1- ITE1008 OPEN SOURCE PROGRAMMING <br>";
+        #OSP J Component
+        echo "A1- ITE1008 OPEN SOURCE PROGRAMMING <br>"; 
+        #B1 slot
+        echo "B1- ITE2002 OPERATING SYSTEMS <br>";
+        #OS LAB slot
+        echo "L41+L42- ITE2002 OPERATING SYSTEMS <br>";
+        #C1 slot
+        echo "C1- MAT3004 APPLIED LINEAR ALGEBRA <br>";
+        #D1 slot
+        echo "D1- PHY1701 ENGINEERING PHYSICS <br>";
+        #PHY LAB slot
+        echo "L57+L58- PHY1701 ENGINEERING PHYSICS <br>";
+        #E1 slot
+        echo "E1- STS2202 SOFT SKILLS <br>"; 
+        #F1 slot
+        echo "F1- ITE1006 THEORY OF COMPUTATION <br>";
+        #G1 slot
+        echo "G1- ITE1007 OBJECT ORIENTED ANALYSIS AND DESIGN <br>";
+        #OOAD J Component
+        echo "G1- ITE1007 OBJECT ORIENTED ANALYSIS AND DESIGN <br>";
+        break;
+}
+?>
